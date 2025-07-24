0bfa5da027d5d41e8f23593bd06806998c4d1f32
yakasiriprathima <yakasiriprathima6@gmail.com>
2025-07-24 18:55:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -505,4 +505,4 @@ if __name__ == '__main__':
         webbrowser.open('http://127.0.0.1:5000')
     
     threading.Timer(1, open_browser).start()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
